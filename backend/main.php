c7f626970d8c1b06258bfb8218f950bacb3d353b
Nejc Ribic <nejc.ribic95@gmail.com>
2017-11-30 21:39:39 +0100

    PHP script for reading and parsing CSV to JSON

--- backend/main.php
+++ backend/main.php
@@ -1,5 +1,32 @@
 <?php
-$file = "../data/testData.csv";
-$csv = file_get_contents($file);
-$array = array_map("str_getcsv", explode("\n", $csv));
-echo json_encode($array);
+//title, date, username, lat, lng, description
+
+$file = fopen('../data/testData.csv', 'r');
+$lineNumber = 0;
+$allDataArray = array();
+$attributes = array();
+while (($line = fgetcsv($file)) !== FALSE) {
+    if ($lineNumber == 0) {
+        $attributes = $line;
+        for ($i = 0; $i < sizeof($attributes); $i++) {
+            $attributes[$i] = ltrim($attributes[$i]."");
+        }
+    }else {
+        $i = 0;
+        $lineDataArray = array();
+        foreach ($line as $element){
+            //replace first spaces in attribute value
+            $element = ltrim($element);
+            $attribute = $attributes[$i];
+            if ($attribute == "lat" || $attribute == "lng") {
+                $element = floatval($element);
+            }
+            $lineDataArray[$attribute] = $element;
+            $i++;
+        }
+        array_push($allDataArray, $lineDataArray);
+    }
+    $lineNumber++;
+}
+fclose($file);
+echo json_encode($allDataArray);
