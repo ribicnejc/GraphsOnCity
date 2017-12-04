8586af3e25ddd053d703c989bb97add32342474d
Nejc Ribic <nejc.ribic95@gmail.com>
2017-12-04 13:40:49 +0100

    Fixed JS graph draw algorithm

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,9 @@
 var map;
 
-
 function reqListner() {
     var content = JSON.parse(this.responseText);
     findPaths(content);
 }
-
 
 function findPaths(data) {
     var paths = {};
@@ -16,20 +14,13 @@ function findPaths(data) {
         coordTuple.lat = userLine.lat;
         coordTuple.lng = userLine.lng;
 
-        if (paths[usernme] === undefined) {
-            paths[usernme] = [];
-            var tmp = paths[usernme];
-            tmp.push(coordTuple);
-            paths[usernme] = tmp;
-        } else {
-            var tmp2 = paths[usernme];
-            tmp2.push(coordTuple);
-            paths[usernme] = tmp2;
-        }
+        if (paths[usernme] === undefined) paths[usernme] = [];
+
+        var tmp = paths[usernme];
+        tmp.push(coordTuple);
+        paths[usernme] = tmp;
     }
 
-
-    //TODO paths.username is actually on attr, so I have to make HASHMap by username and then iterate over and draw graph!!!
     for (var username in paths) {
         if (paths.hasOwnProperty(username)) {
             if (paths[username] === undefined) continue;
@@ -38,20 +29,16 @@ function findPaths(data) {
     }
 }
 
-
-
 function drawGraph(pathData) {
     var userPath = new google.maps.Polyline({
         path: pathData,
         geodesic: true,
         strokeColor: '#0000FF',
         strokeOpacity: 1.0,
-        strokeWeight: 1,
-        map: map
+        strokeWeight: 1
     });
-    // userPath.setMap(map);
+    userPath.setMap(map);
 }
-
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListner);
@@ -59,26 +46,11 @@ oReq.open("GET", "" +
     "backend/main.php");
 oReq.send();
 
-
 function initMap() {
-    // zoom: 13,
-    // center: {lat: 46.0548178, lng: 14.5042642},
     map = new google.maps.Map(document.getElementById('map'), {
-        // zoom: 3,
-        // center: {lat: 0, lng: -180},
         zoom: 13,
         center: {lat: 46.0548178, lng: 14.5042642},
         mapTypeId: 'terrain'
     });
 
-}
-
-function getTestCoordinates() {
-    var flightPlanCoordinates = [
-        {lat: 37.772, lng: -122.214},
-        {lat: 21.291, lng: -157.821},
-        {lat: -18.142, lng: 178.431},
-        {lat: -27.467, lng: 153.027}
-    ];
-    return flightPlanCoordinates;
 }
