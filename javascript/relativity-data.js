7772ffc7ab261c54e2c95b548e7ce38d6610bd68
Nejc Ribic <nejc.ribic95@gmail.com>
2018-02-27 16:23:14 +0100

    relativity functions separated from script.js to file relativity-data.js

---
+++ javascript/relativity-data.js
@@ -0,0 +1,69 @@
+function runAndDrawRelativeData(mainPathsLocal) {
+    var relativeRepetition = [];
+    var relativeLength = [];
+
+    for(var i in relativityPathRepetition) {
+        if(relativityPathRepetition.hasOwnProperty(i)) {
+            relativeRepetition.push(parseInt(i));
+        }
+    }
+
+    for(var j in relativityPathLength) {
+        if (relativityPathLength.hasOwnProperty(j)) {
+            relativeLength.push(parseInt(j))
+        }
+    }
+
+    relativeRepetition = sortNumbersAsc(relativeRepetition);
+    relativeLength = sortNumbersAsc(relativeLength);
+
+    var relativeRepetitionValue1 = $('#slider-path-repetition').slider("values", 0);
+    var relativeRepetitionValue2 = $('#slider-path-repetition').slider("values", 1);
+
+    var relativeLengthValue1 = $('#slider-path-length').slider("values", 0);
+    var relativeLengthValue2 = $('#slider-path-length').slider("values", 1);
+
+    var repetition = [0, 0];
+    var length = [0, 0];
+
+    repetition[0] = Math.floor((relativeRepetition.length * relativeRepetitionValue1) / 100);
+    repetition[1] = Math.floor((relativeRepetition.length * relativeRepetitionValue2) / 100);
+
+    if (repetition[1] === relativeRepetition.length) {
+        repetition[1] = repetition[1] - 1;
+    }
+    if (repetition[0] === relativeRepetition.length) {
+        repetition[0] = repetition[0] - 1;
+    }
+    repetition[0] = relativeRepetition[repetition[0]];
+    repetition[1] = relativeRepetition[repetition[1]];
+
+
+    length[0] = Math.floor((relativeLength.length * relativeLengthValue1) / 100);
+    length[1] = Math.floor((relativeLength.length * relativeLengthValue2) / 100);
+    if (length[1] === relativeLength.length) {
+        length[1] = length[1] - 1;
+    }
+    if (length[0] === relativeLength.length) {
+        length[0] = length[0] - 1;
+    }
+    length[0] = relativeLength[length[0]];
+    length[1] = relativeLength[length[1]];
+
+
+
+    for (var pathKEY in mainPathsLocal) {
+        if (mainPathsLocal.hasOwnProperty(pathKEY)) {
+            var pathToDrawOn = mainPathsLocal[pathKEY];
+            var path = pathToDrawOn["PATH"];
+            var numOfSamePaths = pathToDrawOn["SAME_PATH_NUM"];
+            var pathLength = path.length;
+
+            if (numOfSamePaths >= repetition[0] && numOfSamePaths <= repetition[1]) {
+                if (pathLength >= length[0] && pathLength <= length[1]) {
+                    drawGraph(path, numOfSamePaths);
+                }
+            }
+        }
+    }
+}
--- javascript/script.js
+++ javascript/script.js
@@ -184,10 +184,10 @@ function parseResponse(data) {
                 drawGraph(path2, numOfSamePaths);
         }
     }
-    firstLoad = false
+    firstLoad = false;
 
     if (isRelativeToggleChecked()) {
-        runAndDrawRelativeData();
+        runAndDrawRelativeData(mainPaths);
     }
 
     hideLoadingLayout();
@@ -236,7 +236,7 @@ function addMarker(coords, placeName) {
     if (isShowMarkersChecked())
         marker.setMap(map);
     else marker.setMap(null);
-    
+
     markers.push(marker);
 
 }
@@ -548,74 +548,4 @@ function updateDateSelections() {
     date2 = date2[4] + date2[5] + "/" + date2[6] + date2[7] + "/" + date2[0] + date2[1] + date2[2] + date2[3];
     document.getElementById("datepicker-8").value = date1;
     document.getElementById("datepicker-9").value = date2;
-}
-
-function runAndDrawRelativeData() {
-    var relativeRepetition = [];
-    var relativeLength = [];
-
-    for(var i in relativityPathRepetition) {
-        if(relativityPathRepetition.hasOwnProperty(i)) {
-            relativeRepetition.push(parseInt(i));
-        }
-    }
-
-    for(var j in relativityPathLength) {
-        if (relativityPathLength.hasOwnProperty(j)) {
-            relativeLength.push(parseInt(j))
-        }
-    }
-
-    relativeRepetition = sortNumbersAsc(relativeRepetition);
-    relativeLength = sortNumbersAsc(relativeLength);
-
-    var relativeRepetitionValue1 = $('#slider-path-repetition').slider("values", 0);
-    var relativeRepetitionValue2 = $('#slider-path-repetition').slider("values", 1);
-
-    var relativeLengthValue1 = $('#slider-path-length').slider("values", 0);
-    var relativeLengthValue2 = $('#slider-path-length').slider("values", 1);
-
-    var repetition = [0, 0];
-    var length = [0, 0];
-
-    repetition[0] = Math.floor((relativeRepetition.length * relativeRepetitionValue1) / 100);
-    repetition[1] = Math.floor((relativeRepetition.length * relativeRepetitionValue2) / 100);
-
-    if (repetition[1] === relativeRepetition.length) {
-        repetition[1] = repetition[1] - 1;
-    }
-    if (repetition[0] === relativeRepetition.length) {
-        repetition[0] = repetition[0] - 1;
-    }
-    repetition[0] = relativeRepetition[repetition[0]];
-    repetition[1] = relativeRepetition[repetition[1]];
-
-
-    length[0] = Math.floor((relativeLength.length * relativeLengthValue1) / 100);
-    length[1] = Math.floor((relativeLength.length * relativeLengthValue2) / 100);
-    if (length[1] === relativeLength.length) {
-        length[1] = length[1] - 1;
-    }
-    if (length[0] === relativeLength.length) {
-        length[0] = length[0] - 1;
-    }
-    length[0] = relativeLength[length[0]];
-    length[1] = relativeLength[length[1]];
-
-
-
-    for (var pathKEY in mainPaths) {
-        if (mainPaths.hasOwnProperty(pathKEY)) {
-            var pathToDrawOn = mainPaths[pathKEY];
-            var path = pathToDrawOn["PATH"];
-            var numOfSamePaths = pathToDrawOn["SAME_PATH_NUM"];
-            var pathLength = path.length;
-
-            if (numOfSamePaths >= repetition[0] && numOfSamePaths <= repetition[1]) {
-                if (pathLength >= length[0] && pathLength <= length[1]) {
-                    drawGraph(path, numOfSamePaths);
-                }
-            }
-        }
-    }
 }
