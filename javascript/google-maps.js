f57c9265992e2bc0fa0d68be0dae7c61c7e7634d
Nejc Ribic <nejc.ribic95@gmail.com>
2018-03-17 15:13:35 +0100

    for future rearranging project structure

---
+++ javascript/google-maps.js
@@ -0,0 +1,15 @@
+//TODO here add all google maps functions
+
+// function addMarker(coords, placeName) {
+//     var marker = new google.maps.Marker({
+//         position: coords,
+//         title: placeName
+//     });
+//     marker.addListener('click', function () {
+//         alert(placeName);
+//     });
+//
+//     marker.setMap(map);
+//     markers.push(marker);
+//
+// }
