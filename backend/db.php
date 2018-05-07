e245e228426724e5977a6e0697d3d56854498be6
Nejc Ribic <nejc.ribic95@gmail.com>
2018-05-07 21:32:48 +0200

    restructuring DB connector for easier deployment target chooser

--- backend/db.php
+++ backend/db.php
@@ -2,38 +2,54 @@
 
 class db
 {
-    // Host
-//    private $dbhost = "localhost";
-//    private $dbuser = "nejcribic_nejc";
-//    private $dbpass = "*piF5(QE4xlKw";
-//    private $dbname = "nejcribic_test";
 
-    // Local
-    private $dbhost = "localhost";
-    private $dbuser = "root";
-    private $dbpass = "";
-    private $dbname = "nejcribic_test";
+    private $DEPLOY_TARGET = 0;
 
-    //BerryPi
-//    private $dbhost = "localhost";
-//    private $dbuser = "phpmyadmin";
-//    private $dbpass = "ribic";
-//    private $dbname = "nejcribic_test";
-
-    //Server
-//    private $dbhost = "localhost";
-//    private $dbuser = "root";
-//    private $dbpass = "ribic";
-//    private $dbname = "nejcribic_test";
-
-
+    private $dbhost = null;
+    private $dbuser = null;
+    private $dbpass = null;
+    private $dbname = null;
 
     // Connect
     public function connect()
     {
+        $this->setDeploymentTarget($this->DEPLOY_TARGET);
         $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
         $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbConnection;
+    }
+
+    private function setDeploymentTarget($DEPLOYMENT_TARGET) {
+        switch ($DEPLOYMENT_TARGET) {
+            case 0:
+                //LOCALHOST
+                $this->dbhost = "localhost";
+                $this->dbuser = "root";
+                $this->dbpass = "";
+                $this->dbname = "nejcribic_test";
+                break;
+            case 1:
+                //HOST
+                $this->dbhost = "localhost";
+                $this->dbuser = "nejcribic_nejc";
+                $this->dbpass = "*piF5(QE4xlKw";
+                $this->dbname = "nejcribic_test";
+                break;
+            case 2:
+                //BERRY PI
+                $this->dbhost = "localhost";
+                $this->dbuser = "phpmyadmin";
+                $this->dbpass = "ribic";
+                $this->dbname = "nejcribic_test";
+                break;
+            case 3:
+                //HOME SERVER
+                $this->dbhost = "localhost";
+                $this->dbuser = "root";
+                $this->dbpass = "ribic";
+                $this->dbname = "nejcribic_test";
+                break;
+        }
     }
 }
